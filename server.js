d3d166c7b2cfb3bfbbb1f42906e3678355008774
duytin90 <duytin90@gmail.com>
2016-09-24 18:33:37 -0500

    fixed typos

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-vr express = require("express");
-var path - require("path");
+var express = require("express");
+var path = require("path");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
