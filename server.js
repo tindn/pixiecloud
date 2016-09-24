5170f7515196425b6f82a17d6f2aa813f41e0419
duytin90 <duytin90@gmail.com>
2016-09-24 18:36:22 -0500

    fixed bd url

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 var db;
 
-mongodb.MongoClient.connect(process.env.MONGODB.URI, function (err, database) {
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
 	if (err) {
 		console.log(err);
 		process.exit(1);
