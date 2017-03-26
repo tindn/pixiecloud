ee2a489a2c1c39f33b79f80bc8ab4e67d5eb9a49
Tin Nguyen <tnguyen@qci.com>
2017-03-25 23:21:17 -0500

    added CORS

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ var ObjectID = mongodb.ObjectID;
 var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-// app.use(function(req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 var db;
 var livestrongRepository;
